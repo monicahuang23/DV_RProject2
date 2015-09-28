84763e7fdbf239751c476e3b650d00322b5910ee
Rachel Arthur <rarthur06@yahoo.com>
2015-09-28 18:49:17 -0500

    df code

---
+++ 01 Data/Titanic Analysis.R
@@ -0,0 +1,5 @@
+# Creates the titanic dataframe where all data is included
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from hospital_general_information"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+a <- subset(df, STATE == "TX" & EMERGENCY_SERVICES == "TRUE")
+head(df)
