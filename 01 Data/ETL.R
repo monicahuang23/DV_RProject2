58047c1cc3573171f408e53f7c3e8ae126cf59a4
Monica Huang <monicahuang@wireless-10-146-27-102.public.utexas.edu>
2015-09-28 19:12:47 -0500

    no message

--- 01 Data/ETL.R
+++ 01 Data/ETL.R
@@ -15,36 +15,25 @@ measures <- c("Provider_ID", "Zip_Code", "Phone_Number")
 # Get rid of special characters in each column.
 # Google ASCII Table to understand the following:
 for(n in names(df)) {
-    df[n] <- data.frame(lapply(df[n], gsub, pattern="[^ -~]",replacement= ""))
+  df[n] <- data.frame(lapply(df[n], gsub, pattern="[^ -~]",replacement= ""))
 }
 
 dimensions <- setdiff(names(df), measures)
-if( length(measures) > 1 || ! is.na(dimensions)) {
-  for(d in dimensions) {
+#if( length(measures) > 1 || ! is.na(dimensions)) {
+ # for(d in dimensions) {
     # Get rid of " and ' in dimensions.
-    df[d] <- data.frame(lapply(df[d], gsub, pattern="[\"']",replacement= ""))
+    #df[d] <- data.frame(lapply(df[d], gsub, pattern="[\"']",replacement= ""))
     # Change & to and in dimensions.
-    df[d] <- data.frame(lapply(df[d], gsub, pattern="&",replacement= " and "))
+    #df[d] <- data.frame(lapply(df[d], gsub, pattern="&",replacement= " and "))
     # Change : to ; in dimensions.
-    df[d] <- data.frame(lapply(df[d], gsub, pattern=":",replacement= ";"))
-  }
-}
+  #  df[d] <- data.frame(lapply(df[d], gsub, pattern=":",replacement= ";"))
+  #}
+#}
 
 library(lubridate)
-# Fix date columns, this needs to be done by hand because | needs to be correct.
-#                                                        \_/
-df$Order_Date <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Order_Date), tz="UTC")))
-df$Ship_Date  <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Ship_Date),  tz="UTC")))
-
-# The following is an example of dealing with special cases like making state abbreviations be all upper case.
-# df["State"] <- data.frame(lapply(df["State"], toupper))
 
 # Get rid of all characters in measures except for numbers, the - sign, and period.dimensions
-if( length(measures) > 1 || ! is.na(measures)) {
-  for(m in measures) {
-    df[m] <- data.frame(lapply(df[m], gsub, pattern="[^--.0-9]",replacement= ""))
-  }
-}
+
 
 write.csv(df, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
 
