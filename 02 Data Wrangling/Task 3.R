d2c66ddcc8eba078968f3efcaee99aef69fd64cd
Monica Huang <monicahuang@Monicas-MacBook-Pro.local>
2015-10-02 18:00:57 -0500

    no message

--- 02 Data Wrangling/Task 3.R
+++ 02 Data Wrangling/Task 3.R
@@ -1,11 +1,14 @@
 require(tidyr)
 require(dplyr)
 require(ggplot2)
+require (jsonlite)
+require (RCurl)
 
 # Creates the dataframe where all data is included
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from hospital_general_information"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-View(df)
+
 x <- df %>% group_by (STATE) %>% mutate (TOTAL = n(), LESS_THAN_5 = between (TOTAL, 0, 5), LESS_THAN_10 = between (TOTAL, 0, 10)) %>% filter(EMERGENCY_SERVICES == "true", LESS_THAN_10 == "TRUE") %>%  tbl_df
+
 head(x)
 
 #df %>% group_by (STATE) %>% mutate (TOTAL = n(), LESS_THAN_10 = between (TOTAL, 0, 10), LESS_THAN_20 = between (TOTAL, 0, 20)) %>% filter(EMERGENCY_SERVICES == "true", LESS_THAN_20 == "TRUE") %>% ggplot(aes(x=STATE, y=HOSPITAL_OWNERSHIP, color=as.character(LESS_THAN_10))) + geom_point(position = position_jitter()) + facet_wrap(~LESS_THAN_10)
