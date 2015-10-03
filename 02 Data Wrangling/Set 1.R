278121bef78a11f87e668f1f0e14a73bdcf78512
Monica Huang <monicahuang@wireless-10-146-39-223.public.utexas.edu>
2015-10-03 11:30:17 -0500

    no message

--- 02 Data Wrangling/Set 1.R
+++ 02 Data Wrangling/Set 1.R
@@ -3,6 +3,9 @@ require(dplyr)
 require(ggplot2)
 require (jsonlite)
 require (RCurl)
+
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from hospital_general_information"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
 x <- df %>% group_by(STATE, HOSPITAL_TYPE) %>% summarise(n = n())
 
 plot1 <- ggplot() + 
--- 02 Data Wrangling/Set 2.R
+++ 02 Data Wrangling/Set 2.R
@@ -1,3 +1,11 @@
+require(tidyr)
+require(dplyr)
+require(ggplot2)
+require (jsonlite)
+require (RCurl)
+
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from hospital_general_information"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
 x <- df %>% filter(HOSPITAL_TYPE == "Childrens") %>% group_by(STATE, HOSPITAL_TYPE, HOSPITAL_OWNERSHIP) %>% summarise(n = n())
 
 plot2 <- ggplot() + 
